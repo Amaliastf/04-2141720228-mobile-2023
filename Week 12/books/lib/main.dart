bbe33b294f7d54904efa1498faaecae22b0f1278
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-11-19 15:29:23 +0700

    W12 Praktikum 2 Soal 1

--- Week 12/books/lib/main.dart
+++ Week 12/books/lib/main.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart'
+as http;
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -29,16 +32,18 @@ class MyApp extends StatelessWidget {
 }
 
 class FuturePage extends StatefulWidget {
-  const FuturePage({super.key});
+  const FuturePage({
+    super.key
+  });
 
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
+  // class MyHomePage extends StatefulWidget {
+  //   const MyHomePage({super.key, required this.title});
 
-//   final String title;
+  //   final String title;
 
-// Praktikum 1 Langkah 3
-@override
-  State<FuturePage> createState() => _FuturePageState();
+  // Praktikum 1 Langkah 3
+  @override
+  State < FuturePage > createState() => _FuturePageState();
 }
 
 
@@ -46,7 +51,7 @@ class FuturePage extends StatefulWidget {
 //   State<MyHomePage> createState() => _MyHomePageState();
 // }
 
-class _FuturePageState extends State<FuturePage> {
+class _FuturePageState extends State < FuturePage > {
   // Praktikum 1 Langkah 3
   String result = '';
   // int _counter = 0;
@@ -66,71 +71,85 @@ class _FuturePageState extends State<FuturePage> {
       ),
       body: Center(
         child: Column(children: [
-          const Spacer(),
-          // ElevatedButton(
-          //   child: const Text('GO!'),
-          //   onPressed: () {},
+            const Spacer(),
+            // Praktikum 2 Langkah 3
+              ElevatedButton(
+                child: const Text('GO!'),
+                onPressed: () {
+                  count();
+                },
+              ),
+              // // Praktikum 1 Langkah 5 Soal Nomor 3
+              // ElevatedButton(
+              //   child: const Text('GO!'),
+              //     onPressed: () {
+              //       setState(() {});
+              //       getData().then((value) {
+              //         result = value.body.toString().substring(0, 450);
+              //         setState(() {});
+              //       }).catchError((_) {
+              //         result = 'An error occurred';
+              //         setState(() {});
+              //       });
+              //     },
+              // ),
+              const Spacer(),
+                Text(result),
+                const Spacer(),
+                  const CircularProgressIndicator(),
+                    const Spacer(),
+          ]
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: <Widget>[
+          //       const Text(
+          //         'You have pushed the button this many times:',
+          //       ),
+          //       Text(
+          //         '$_counter',
+          //         style: Theme.of(context).textTheme.headlineMedium,
+          //       ),
+          //     ],
+          //   ),
           // ),
-          // Praktikum 1 Langkah 5 Soal Nomor 3
-          ElevatedButton(
-              child: const Text('GO!'),
-              onPressed: () {
-                setState(() {});
-                getData().then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {});
-                }).catchError((_) {
-                  result = 'An error occurred';
-                  setState(() {});
-                });
-              },
-            ),
-          const Spacer(),
-          Text(result),
-          const Spacer(),
-          const CircularProgressIndicator(),
-          const Spacer(),
-        ]
-      //     mainAxisAlignment: MainAxisAlignment.center,
-      //     children: <Widget>[
-      //       const Text(
-      //         'You have pushed the button this many times:',
-      //       ),
-      //       Text(
-      //         '$_counter',
-      //         style: Theme.of(context).textTheme.headlineMedium,
-      //       ),
-      //     ],
-      //   ),
-      // ),
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: _incrementCounter,
+          //   tooltip: 'Increment',
+          //   child: const Icon(Icons.add),
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
   // Praktikum 1 Soal Nomor 2
-  Future<Response> getData() async {
+  Future < Response > getData() async {
     const authority = 'www.googleapis.com';
     const path = '/books/v1/volumes/F9lxDQAAQBAJ';
     Uri url = Uri.https(authority, path);
     return http.get(url);
   }
 
-  // ElevatedButton(
-  //   child: Text('GO!'),
-  //   onPressed: (){
-  //     setState(() {});
-  //     getData()
-  //     .then((value) {
-  //       result = value.body.toString().substring(0, 450);
-  //       setState(() {});
-  //     }).catchError((_)){
-  //       result = 'An error occured';
-  //       setState(() {});
-  //     });
-  //   },
-  // ),
+  // Praktikum 2 Langkah 1
+  Future < int > returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future < int > returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future < int > returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+  // Praktikum 2 Langkah 2
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
 }
