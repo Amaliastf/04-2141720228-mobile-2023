eaaadfcdacf520dcf98a9af4bf273a42a8617ddc
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-11-20 15:31:50 +0700

    W12 Praktikum 7 Soal 13

--- Week 12/books/lib/geolocation.dart
+++ Week 12/books/lib/geolocation.dart
@@ -6,44 +6,73 @@ class LocationScreen extends StatefulWidget{
 
   @override
   State<LocationScreen> createState() => _LocationScreenState();
+
 }
 
 class _LocationScreenState extends State<LocationScreen> {
+    // Praktikum 7 Langkah 2
+  Future<Position>? position;
   String myPosition = '';
 
   @override
   void initState() {
+    // Praktikum 7 Langkah 3
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition = 'Latitude: ${myPos.latitude.toString()} - Longitude:{myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
+    // getPosition().then((Position myPos) {
+    //   myPosition = 'Latitude: ${myPos.latitude.toString()} - Longitude:{myPos.longitude.toString()}';
+    //   setState(() {
+    //     myPosition = myPosition;
+    //   });
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    // Praktikum 6 Langkah 8
-    final myWidget = myPosition == ''
-      ? const CircularProgressIndicator()
-      : Text(myPosition);;
-
+    // Praktikum 7 Langkah 4
     return Scaffold(
       appBar: AppBar(title: Text('Current Location')),
-      body: Center(child: myWidget),
+      body: Center(child: FutureBuilder(
+        future: position,
+        builder: (BuildContext context, AsyncSnapshot<Position>
+          snapshot) {
+            if (snapshot.connectionState ==
+              ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+              else if (snapshot.connectionState == 
+                ConnectionState.done) {
+                  return Text(snapshot.data.toString());
+                }
+                else {
+                  return const Text('');
+                }
+          },
+      ),
+      )
     );
+    // // Praktikum 6 Langkah 8
+    // final myWidget = myPosition == ''
+    //   ? const CircularProgressIndicator()
+    //   : Text(myPosition);;
+
+    // return Scaffold(
+    //   appBar: AppBar(title: Text('Current Location')),
+    //   body: Center(child: myWidget),
+    // );
     // return Scaffold(
     //   appBar: AppBar(title: const Text('Current Location')),
     //   body: Center(child: Text(myPosition)),
     // );
   }
   Future<Position> getPosition() async {
-    await Geolocator.requestPermission();
+    // await Geolocator.requestPermission();
+    // Praktikum 7 Langkah 1
     await Geolocator.isLocationServiceEnabled();
     await Future.delayed(const Duration(seconds: 3));
-    Position? position = 
-      await Geolocator.getCurrentPosition();
+    Position position = await Geolocator. getCurrentPosition();
+    // Position? position = 
+    //   await Geolocator.getCurrentPosition();
       return position;
   }
 
