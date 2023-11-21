08e911b48162772944e21e7aba36fb748e9e7fa7
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-11-21 14:47:55 +0700

    W13: Jawaban Soal 2

---
+++ Week 13/stream_nama/lib/stream.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorSteam {
+  final List < Color > colors = [
+    const Color.fromARGB(255, 161, 124, 94),
+    Colors.blueGrey,
+    const Color.fromARGB(255, 132, 84, 141),
+    const Color.fromARGB(255, 105, 131, 120),
+    const Color.fromARGB(255, 87, 88, 84)
+    // Colors.blueGrey,
+    // Colors.amber,
+    // Colors.deepPurple,
+    // Colors.lightBlue,
+    // Colors.teal
+  ];
+}
